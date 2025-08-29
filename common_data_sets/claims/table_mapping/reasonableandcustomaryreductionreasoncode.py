d608146009bd9772c0332c337ea8f9aad062dee1
manek1 <42269298+manek1@users.noreply.github.com>
2025-08-29 20:42:20 +0530

    Create reasonableandcustomaryreductionreasoncode.py

---
+++ common_data_sets/claims/table_mapping/reasonableandcustomaryreductionreasoncode.py
@@ -0,0 +1,18 @@
+from common_data_sets.common.abstract_glue_table_mapping import AbstractGlueTableMapping
+from pyspark.sql.types import StringType, StructField, StructType
+from common_data_sets.common.configs import config
+
+
+class ReasonableAndCustomaryReductionReasonCode(AbstractGlueTableMapping):
+    table_name = "reasonableandcustomaryreductionreasoncode"
+    schema_name = config.IDS_SCHEMA
+    distinct_flag = True
+    schema = StructType([StructField('reasonableandcustomaryreductionreasoncode', StringType(), True),
+                         StructField('reasonableandcustomaryreductionreason', StringType(), True)
+                         ])
+
+
+def reasonableandcustomaryreductionreasoncode(*args, **kwargs):
+    rsn = ReasonableAndCustomaryReductionReasonCode()
+    rsn.initialize_table(**kwargs)
+    return rsn
