32d8378cc4d7e677effa21ae1cfc193baaa1afa5
manek1 <42269298+manek1@users.noreply.github.com>
2025-08-29 22:58:30 +0530

    Create country.py

---
+++ eligibility/table_mapping/country.py
@@ -0,0 +1,19 @@
+from common_data_sets.common.abstract_glue_table_mapping import AbstractGlueTableMapping
+from pyspark.sql.types import StringType, StructField, StructType, TimestampType
+from common_data_sets.common.configs import config
+
+
+class Country(AbstractGlueTableMapping):
+    schema_name = config.IDS_SCHEMA
+    table_name = "country"
+    distinct_flag = True
+    schema = StructType([
+        StructField('countrycode', StringType(), True),
+        StructField('country', StringType(), True)
+    ])
+
+
+def country(*args, **kwargs):
+    cor = Country()
+    cor.initialize_table(**kwargs)
+    return cor
