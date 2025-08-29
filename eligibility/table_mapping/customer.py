04696e8b494abaab520b4ac6caa23eea221f5573
manek1 <42269298+manek1@users.noreply.github.com>
2025-08-29 23:01:05 +0530

    Create customer.py

---
+++ eligibility/table_mapping/customer.py
@@ -0,0 +1,20 @@
+from common_data_sets.common.abstract_glue_table_mapping import AbstractGlueTableMapping
+from pyspark.sql.types import StringType, StructField, StructType, TimestampType
+from common_data_sets.common.configs import config
+
+
+class Customer(AbstractGlueTableMapping):
+    schema_name = config.IDS_SCHEMA
+    table_name = "customer"
+    schema = StructType([StructField("customerid", StringType(), True),
+                         StructField("dateofbirth", TimestampType(), True),
+                         StructField('gendercode', StringType(), True),
+                         StructField('homecountrycode', StringType(), True),
+                         StructField('partyid', StringType(), True),
+                         StructField('sourcesystemid', StringType(), True)])
+
+
+def customer(*args, **kwargs):
+    cstmr = Customer()
+    cstmr.initialize_table(**kwargs)
+    return cstmr
