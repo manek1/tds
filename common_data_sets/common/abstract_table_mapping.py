1a203fbbdf1904a1019b63b528869803944b5bba
manek1 <42269298+manek1@users.noreply.github.com>
2025-10-23 12:15:03 +0530

    Implement AbstractTableMapping class for table mapping

---
+++ common_data_sets/common/abstract_table_mapping.py
@@ -0,0 +1,40 @@
+import json
+from abc import ABC, abstractmethod
+from pyspark.sql import types as T
+
+
+class AbstractTableMapping(ABC):
+    @property
+    @abstractmethod
+    def table_name(self) -> str:
+        """Name of the table"""
+
+    @property
+    @abstractmethod
+    def schema_name(self):
+        pass
+
+    @property
+    @abstractmethod
+    def key_columns(self):
+        pass
+
+    @property
+    @abstractmethod
+    def schema(self) -> T.StructType:
+        """
+        Store the schema of the table using pyspark's StructType class.
+        :return: T.StructType representing the schema.
+        """
+
+    @property
+    def __repr__(self) -> str:
+        """
+        The string representation of a table mapping is its full table name.
+        :return: The full table name.
+        """
+        return "`" + self.schema_name + "`.`" + self.table_name + "`"
+
+    @abstractmethod
+    def initialize_table(self, **kwargs):
+        pass
